0987240e5e460d24c81c31525b8cb114cb6321fa
mekwon92 <me92100984@gmail.com>
2024-09-05 22:54:32 +0900

    9/5 home

--- lesson07_oop/src/lesson07_oop/Insertion.java
+++ lesson07_oop/src/lesson07_oop/Insertion.java
@@ -1,12 +1,39 @@
 package lesson07_oop;
+
+import java.util.Arrays;
+
 //삽입정렬
 public class Insertion {
 	public static void main(String[] args) {
+		int[] arr = {4,3,2,1};
 		//작은 인덱스에서부터 시작(회차)
 		//내가 더 작으면.... arraycopy...
 		//현재 위치의 인덱스 미만의 값들과 대소비교 (반복문필요 내가 들어갈 곳의 위치..)
+		/*
+		 * 	  {4,3,2,1}
+		 * 1. {3,4,2,1} 1~0
+		 * 2. {3,2,4,1} 
+		 * 3. {2,3,4,1} 2~0
+		 *     1 2 3 4
+		 * 				3~0
+		 * if(arr[0]>arr[1])*/
+		
+		for(int i = 1; i < arr.length - 1; i++) {
+			for(int j = i; j >= 0 ; j--) {
+				if(arr[i-1]>arr[i]) {
+					int tmp;
+					tmp = arr[i];
+					arr[i] = arr[i-1];
+					arr[i-1] = tmp;	
+				}
+			}
+			System.out.println(Arrays.toString(arr));
+
+		}
 		// -조건 검수 후 해당 위치에 삽입 예정
 		// -해당위치에서부터 한칸씩 우측이동
 		// -예정 위치에 해당값 대
+		
+		
 	}
 }
--- lesson07_oop/src/lesson07_oop/Selection.java
+++ lesson07_oop/src/lesson07_oop/Selection.java
@@ -4,22 +4,42 @@ import java.util.Arrays;
 
 public class Selection {
 	public static void main(String[] args) {
-		int[] arr = {5,1,2,0,3,4,6};
+		int[] arr = {6,5,4,3,2,1};
+		
+		//654321
+		
+		//1. 0번인덱스부터 끝까지 한번 훑어서 최소값을 찾아낸다.(1 찾음)
+		//2. 최소값을 0번 인덱스와 바꾼다.
+		//3. 1번인덱스부터 끝까지 훑어서 최소값을 찾아낸다. (2 찾음)
+		//4. 그 최소값을 1번 인덱스와 바꾼다.
+		
+		
+		//회차반복문
 		int min = arr[0];
 		int idx = 0;
-		//회차반복문
-		//최소값 에 0번인 
-		for(int i = 0; i < arr.length ; i++) {	
 		//최소값찾기 반복문 내부
-			for(int j = 0; j < arr.length ; j++) {				
+		for(int i = 0; i <arr.length-1 ; i++) {
+
+			for(int j = 1; j < arr.length ; j++) {				
 				if(min > arr[j]) {
-					min = arr[]
-				}	
+					min = arr[j];
+					idx = j;
+					}
 			}
-		}
-		System.out.println(idx);
-		System.out.println(min);
-		System.out.println(Arrays.toString(arr));
+			int tmp = arr[idx];
+			arr[idx] = arr[i];
+			arr[i] = tmp;
+				System.out.println(Arrays.toString(arr));
+
+			
+	}
+			
+			
+			
+
+				
+			
+				
 		// - 최소값이 존재하는 인덱스
 		
 		// - 0번째 위치의 값이 위 인덱스에 있던 값과 교환
